29c6fe36ae922c5d0a91d712d23b4eaf22220081
TheBen2005 <ben.myers@outlook.com>
2026-02-05 11:31:27 -0700

    Finished debugging stalemate function. Finished Phase 1!

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -161,7 +161,7 @@ public class PieceMovesCalculator {
         upd_col = position.getColumn();
         while (true) {
             upd_row -= 1;
-            if (upd_row <= 0 || upd_col >= 8) {
+            if (upd_row <= 0 || upd_col > 8) {
                 break;
             }
             ChessPiece piece_2 = Board.getPiece(new ChessPosition(upd_row, upd_col));
