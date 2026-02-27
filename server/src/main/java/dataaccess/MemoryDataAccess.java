969c19bf5727930d9139cad7e3472d4606a221ac
TheBen2005 <ben.myers@outlook.com>
2026-02-27 11:37:47 -0700

    Created Skeleton for Memory Data Access class

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,5 +1,62 @@
 package dataaccess;
 
-public interface MemoryDataAccess {
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryDataAccess implements DataAccess{
+    List<UserData> userList = new ArrayList<>();
+    List<GameData> gameList = new ArrayList<>();
+    List<AuthData> AuthData = new ArrayList<>();
+
+
+
+    public void getUser(String username){
+
+    }
+
+    public void createUser(UserData userData){
+
+    }
+
+    public void createAuth(AuthData authData){
+
+    }
+
+    public void deleteAuth(String authToken){
+
+    }
+
+    public void listGames(){
+
+    }
+
+    public void createGame(GameData gameData){
+
+    }
+
+    public void getGame(int gameId){
+
+    }
+
+    public void updateGame(GameData gameData){
+
+    }
+
+    public void clearUsers(){
+
+    }
+
+    public void clearGames(){
+
+    }
+
+    public void clearAuth(){
+
+    }
+
 
 }
