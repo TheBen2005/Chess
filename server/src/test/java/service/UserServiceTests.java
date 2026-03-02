740ce421482007e8491720d3c7a5309aebb0102d
TheBen2005 <ben.myers@outlook.com>
2026-03-02 10:51:56 -0700

    added positive and negative test for register.

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,5 +1,7 @@
 package service;
 
+import dataaccess.DataAccessException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +13,26 @@ public class UserServiceTests {
     public void setup(){
         userService = new UserService();
     }
+
     @Test
-    public void
+    public void registerSuccess(){
+        RegisterRequest registerRequest = new("Ben", "12345", "Ben email");
+        RegisterResult registerResult = userService.register(registerRequest);
+        Assertions.assertNotNull(registerResult.authToken);
+        Assertions.assertEquals("Ben" registerResult.username());
+    }
+
+    public void registerTwice(){
+        try{
+            RegisterRequest registerRequest = new("Ben", "12345", "Ben email");
+            userService.register(registerRequest);
+            userService.register(registerRequest);
+            Assertions.fail("Exception was not thrown")
+        }
+        catch(DataAccessException dataAccessException){
+
+        }
+
+    }
 
 }
