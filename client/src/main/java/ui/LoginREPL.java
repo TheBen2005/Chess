79c4ac7e41bc57ef64ee4e275dd2222305cb6158
TheBen2005 <ben.myers@outlook.com>
2026-03-24 16:30:40 -0600

    Created help function

--- client/src/main/java/ui/LoginREPL.java
+++ client/src/main/java/ui/LoginREPL.java
@@ -18,16 +18,33 @@ public class LoginRepl implements NotificationHandler {
     private String visitorName = null;
     private final ServerFacade server;
     private final WebSocketFacade ws;
-    private State state = State.SIGNEDOUT;
+    private State state = State.PRELOGIN;
 
     public PetClient(String serverUrl) throws ResponseException {
         server = new ServerFacade(serverUrl);
         ws = new WebSocketFacade(serverUrl, this);
     }
+    public enum State {
+        PRELOGIN,
+        POSTLOGIN,
+        GAMEPLAY
+    }
 
     public void run() {
         System.out.println(LOGO + )
+        System.out.print(help());
 
+    }
+    public String help() {
+        if(state == State.PRELOGIN){
+            return """
+                    - register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                    - login <USERNAME> <PASSWORD> - to play chess
+                    - quit - playing chess
+                    - help - with possible commands            
+                    
+                    """;
 
+        }
     }
 }
