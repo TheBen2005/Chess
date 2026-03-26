861beb6c959050afd696dd15c7348862c58d76b2
TheBen2005 <ben.myers@outlook.com>
2026-03-25 18:03:31 -0600

    Started chess drawing

--- client/src/main/java/ui/BoardDrawing.java
+++ client/src/main/java/ui/BoardDrawing.java
@@ -1,4 +1,34 @@
 package ui;
 
 public class BoardDrawing {
+    EscapeSequences escapeSequences = new EscapeSequences();
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++){
+            if(i % 2 == 0){
+                if(j % 2 == 0){
+                    escapeSequences.SET_BG_COLOR_BLACK;
+                }
+                else{
+                    escapeSequences.SET_BG_COLOR_WHITE;
+                }
+            }
+            else {
+                if (j % 2 == 0) {
+                    escapeSequences.SET_BG_COLOR_WHITE;
+                } else {
+                    escapeSequences.SET_BG_COLOR_BLACK;
+                }
+            }
+            if(i == 2){
+                escapeSequences.WHITE_PAWN;
+            }
+            if(i == 7){
+                escapeSequences.BLACK_PAWN;
+            }
+
+
+
+
+        }
+    }
 }
