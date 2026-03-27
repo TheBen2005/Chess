e6bfabf8259a949b20514e7a5fbd8bdf7b315786
TheBen2005 <ben.myers@outlook.com>
2026-03-27 11:23:45 -0600

    Established skeleton for server facade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,5 +1,6 @@
 package client;
 
+import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
 import server.Server;
 
@@ -7,12 +8,14 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -20,10 +23,77 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        facade.clear();
+    }
+
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void loginSuccess() throws Exception{
+
     }
+
+    @Test
+    void loginfailure() throws Exception{
+
+    }
+
+    @Test
+    void registerSuccess() throws Exception{
+
+    }
+
+    @Test
+    void registerfailure() throws Exception{
+
+    }
+
+    @Test
+    void logoutSuccess() throws Exception{
+
+    }
+
+    @Test
+    void logoutFailure() throws Exception{
+
+    }
+
+    @Test
+    void createGameSuccess() throws Exception{
+
+    }
+
+    @Test
+    void createGamefailure() throws Exception{
+
+    }
+
+    @Test
+    void listGamesSuccess() throws Exception{
+
+    }
+
+    @Test
+    void listGamesfailure() throws Exception{
+
+    }
+
+    @Test
+    void joinGameSuccess() throws Exception{
+
+    }
+
+    @Test
+    void joinGameFailure() throws Exception{
+
+    }
+
+    @Test
+    void clear() throws Exception{
+
+    }
+
+
 
 }
