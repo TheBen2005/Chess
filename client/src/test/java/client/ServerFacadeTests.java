596cf5c36e35cdec935ff93289f407551afd03a1
TheBen2005 <ben.myers@outlook.com>
2026-03-27 15:57:51 -0600

    Finished server facade tests.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,11 +1,12 @@
 package client;
 
 import dataaccess.DataAccessException;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import server.Server;
-import service.LoginRequest;
-import service.LogoutRequest;
-import service.RegisterRequest;
+import service.*;
+
+import java.util.List;
 
 
 public class ServerFacadeTests {
@@ -111,32 +112,97 @@ public class ServerFacadeTests {
     }
 
     @Test
-    void createGameSuccess() throws Exception{
+    void createGameSuccess() throws DataAccessException{
+        RegisterRequest registerRequest = new RegisterRequest("Theben", "12345", "password");
+        facade.register(registerRequest);
+        LoginRequest loginRequest = new LoginRequest("Theben", "12345");
+        var authData = facade.login(loginRequest);
+        String authToken = authData.authToken();
+        CreateGameRequest createGameRequest = new CreateGameRequest("game", authToken);
+        facade.createGame(createGameRequest);
+        Assertions.assertNotNull(authToken);
 
     }
 
     @Test
-    void createGamefailure() throws Exception{
+    void createGamefailure() throws DataAccessException{
+        try{
+            RegisterRequest registerRequest = new RegisterRequest("Theben", "12345", "password");
+            facade.register(registerRequest);
+            LoginRequest loginRequest = new LoginRequest("Theben", "12345");
+            var authData = facade.login(loginRequest);
+            CreateGameRequest createGameRequest = new CreateGameRequest("game", "hsbsbsb");
+        }
+        catch(DataAccessException dataAccessException){
+            Assertions.assertNotNull(dataAccessException.getMessage());
+        }
 
     }
 
     @Test
     void listGamesSuccess() throws Exception{
+        RegisterRequest registerRequest = new RegisterRequest("Theben", "12345", "password");
+        facade.register(registerRequest);
+        LoginRequest loginRequest = new LoginRequest("Theben", "12345");
+        var authData = facade.login(loginRequest);
+        String authToken = authData.authToken();
+        ListGamesRequest listGamesRequest = new ListGamesRequest(authToken);
+        ListGamesResult listGamesResult = facade.listGames(listGamesRequest);
+        List<GameData> gameList = listGamesResult.games();
+
 
     }
 
     @Test
-    void listGamesfailure() throws Exception{
+    void listGamesfailure() throws DataAccessException{
+        try{
+            RegisterRequest registerRequest = new RegisterRequest("Theben", "12345", "password");
+            facade.register(registerRequest);
+            LoginRequest loginRequest = new LoginRequest("Theben", "12345");
+            var authData = facade.login(loginRequest);
+            String authToken = authData.authToken();
+            ListGamesRequest listGamesRequest = new ListGamesRequest("jsjbsjbdjdb");
+            ListGamesResult listGamesResult = facade.listGames(listGamesRequest);
+            Assertions.fail("should have thrown an exception");
+        }
+        catch(DataAccessException dataAccessException){
+            Assertions.assertNotNull(dataAccessException.getMessage());
+        }
 
     }
 
     @Test
-    void joinGameSuccess() throws Exception{
-
+    void joinGameSuccess() throws DataAccessException{
+        RegisterRequest registerRequest = new RegisterRequest("Theben", "12345", "password");
+        facade.register(registerRequest);
+        LoginRequest loginRequest = new LoginRequest("Theben", "12345");
+        var authData = facade.login(loginRequest);
+        String authToken = authData.authToken();
+        CreateGameRequest createGameRequest = new CreateGameRequest("game", authToken);
+        CreateGamesResult createGamesResult = facade.createGame(createGameRequest);
+        int gameId = createGamesResult.gameID();
+        JoinGameRequest joinGameRequest = new JoinGameRequest("WHITE", gameId, authToken);
+        facade.joinGame(joinGameRequest);
+        Assertions.assertNotNull(authToken);
     }
 
     @Test
-    void joinGameFailure() throws Exception{
+    void joinGameFailure() throws DataAccessException{
+        try{
+            RegisterRequest registerRequest = new RegisterRequest("Theben", "12345", "password");
+            facade.register(registerRequest);
+            LoginRequest loginRequest = new LoginRequest("Theben", "12345");
+            var authData = facade.login(loginRequest);
+            String authToken = authData.authToken();
+            CreateGameRequest createGameRequest = new CreateGameRequest("game", authToken);
+            CreateGamesResult createGamesResult = facade.createGame(createGameRequest);
+            int gameId = createGamesResult.gameID();
+            JoinGameRequest joinGameRequest = new JoinGameRequest("White", 6, "gdhdvd");
+            facade.joinGame(joinGameRequest);
+        }
+        catch(DataAccessException dataAccessException){
+            Assertions.assertNotNull(dataAccessException.getMessage());
+        }
 
     }
 
